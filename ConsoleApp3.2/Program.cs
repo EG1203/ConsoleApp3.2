42e9c9124164a35a21ad79be67cf21b89c46b72f
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2021-12-02 18:44:28 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3.2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+Random r = new Random();
+
+int n = r.Next(10, 9999);
+
+Console.WriteLine("Натуральное число n: " + n);
+
+Console.WriteLine("Предпоследняя цифра числа n: " + n % 100 / 10);
+
+Console.ReadKey();
